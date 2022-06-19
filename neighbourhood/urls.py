096e18d6d39b4e36b1c7d9418468b7a7dbd53b39
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-06-19 22:58:47 +0300

    install crispy forms, update index templates, update urls

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-  
+    path('',auth_views.LoginView.as_view(template_name='index.html'),name = 'index'),
+    path('register/', views.signup, name='signup'),
 ]
