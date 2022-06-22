105bca0f119e85332b0fb79b1809df8660f371f7
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-06-22 11:42:32 +0300

    add env and gitignore files, update settings

--- blackpearl/settings.py
+++ blackpearl/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--o2ve=yi)j+l_tvhv&g5l0o)-os3n#cg0b$n9&lr7urwc96tgo'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
